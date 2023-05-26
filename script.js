54badc6348f4a6e16115ec5bcb72a006ef1f9e0b
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-26 17:21:14 -0300

    Resquisito 3 concluido, Validação formulario de login

---
+++ script.js
@@ -0,0 +1,19 @@
+const formLogin = document.querySelector('.trybewarts-login');
+
+
+const validaLogin = () => {
+  formLogin.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const inputEmail = document.querySelector('.trybewarts-login #email');
+    const inputPass  = document.querySelector('.trybewarts-login #password');
+    if (inputEmail.value === 'tryber@teste.com' && inputPass.value === '123456') {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Email ou senha inválidos.');
+    }
+
+  });
+};
+
+
+validaLogin();
