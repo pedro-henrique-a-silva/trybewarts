cb8c299a754e4d9620d99d4b31fc59f90e978db6
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-26 18:03:15 -0300

    Resolvendo problemas do lint

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const validaLogin = () => {
   formLogin.addEventListener('submit', (event) => {
     event.preventDefault();
     const inputEmail = document.querySelector('.trybewarts-login #email');
-    const inputPass  = document.querySelector('.trybewarts-login #password');
+    const inputPass = document.querySelector('.trybewarts-login #password');
     if (inputEmail.value === 'tryber@teste.com' && inputPass.value === '123456') {
       alert('Olá, Tryber!');
     } else {
