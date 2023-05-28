7246ae7cc2f4742481313225929d39fd3ed8a680
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-27 21:54:24 -0300

    Requisito 21 concluido, dados do estudando exibidos

--- script.js
+++ script.js
@@ -2,6 +2,47 @@ const formLogin = document.querySelector('.trybewarts-login');
 const agreement = document.querySelector('#agreement');
 const comments = document.querySelector('.form-inputs textarea');
 const counter = document.querySelector('#counter');
+const formStudent = document.querySelector('#evaluation-form');
+const formData = document.querySelector('#form-data');
+
+const getMaterias = (elements) => {
+  const checkedMisterios = [];
+  for (let i = 0; i < elements.length; i += 1) {
+    if (elements[i].checked) {
+      checkedMisterios.push(elements[i].value);
+    }
+  }
+  return checkedMisterios;
+};
+
+const getHouse = (element) => element.options[element.selectedIndex].value;
+
+const getRadio = (elements) => {
+  for (let i = 0; i < elements.length; i += 1) {
+    if (elements[i].checked) {
+      return elements[i].value;
+    }
+  }
+};
+
+const getStudentData = () => {
+  const name = document.querySelector('#input-name').value;
+  const lastName = document.querySelector('#input-lastname').value;
+  const email = document.querySelector('#input-email').value;
+  const house = getHouse(document.querySelector('#house'));
+  const family = getRadio(document.querySelectorAll('.radio-familia input'));
+  const materias = getMaterias(document.querySelectorAll('.checkbox-content input'));
+  const rate = getRadio(document.querySelectorAll('.radio-rate'));
+  const commentsValue = comments.value;
+
+  return [`Nome: ${name} ${lastName}`,
+    `Email: ${email}`,
+    `Casa: ${house}`,
+    `Família: ${family}`,
+    `Matérias: ${String(materias).replaceAll(',', ', ')}`,
+    `Avaliação: ${rate}`,
+    `Observações: ${commentsValue}`];
+};
 
 const validaLogin = () => {
   formLogin.addEventListener('submit', (event) => {
@@ -34,6 +75,23 @@ const countCaractere = () => {
   });
 };
 
+const studentSubmit = () => {
+  formStudent.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const data = getStudentData();
+
+    for (let index = 0; index < data.length; index += 1) {
+      const paragraph = document.createElement('p');
+      paragraph.innerText = data[index];
+      formData.appendChild(paragraph);
+    }
+
+    formStudent.style.display = 'none';
+    formData.style.display = 'flex';
+  });
+};
+
 validaLogin();
 verifyAgreement();
 countCaractere();
+studentSubmit();
